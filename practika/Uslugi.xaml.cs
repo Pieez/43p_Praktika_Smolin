92ef6652e98500bbcd4a94761190da5ece334731
Иван Смолин <ivan.smolin.78@mail.ru>
2023-03-20 12:10:36 +0300

    patch 1.6.9

---
+++ practika/Uslugi.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace practika
+{
+    /// <summary>
+    /// Логика взаимодействия для Uslugi.xaml
+    /// </summary>
+    public partial class Uslugi : Window
+    {
+        public Uslugi()
+        {
+            InitializeComponent();
+        }
+
+        private void AddServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void SaveToWord(List<Service> services)
+        {
+           
+        }
+    }
+}
